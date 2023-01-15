c7be8b2325bcc53ec35a1966d23ee4e73bac1ded
Yohan Kim <yohan@yohanpro.com>
2023-01-15 14:38:40 +0900

    webview Controller사용

--- flutter_webview/lib/screen/home_screen.dart
+++ flutter_webview/lib/screen/home_screen.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  WebViewController? webViewController;
+  final homeUrl = 'https://yohanpro.com';
+
+  HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -11,9 +14,19 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('요한의 블로그'),
         centerTitle: true,
-        backgroundColor: Color(0xFF1E1E1E),
+        backgroundColor: Colors.black,
+        actions: [
+          IconButton(
+              onPressed: () {
+                webViewController!.loadUrl(homeUrl);
+              },
+              icon: Icon(Icons.home, color: Colors.white))
+        ],
       ),
       body: WebView(
+        onWebViewCreated: (WebViewController webViewController) {
+          this.webViewController = webViewController;
+        },
         initialUrl: 'https://yohanpro.com',
         javascriptMode: JavascriptMode.unrestricted,
       ),

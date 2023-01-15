979e14dbc18f79e76dfd2690002f57bb88eec245
Yohan Kim <yohan@yohanpro.com>
2023-01-15 14:38:40 +0900

    webview 사용해보기

--- flutter_webview/lib/screen/home_screen.dart
+++ flutter_webview/lib/screen/home_screen.dart
@@ -1,41 +1,20 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
-  WebViewController? controller;
-  final homeUrl = 'https://blog.codefactory.ai';
-
-  HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.orange,
-        title: Text('Code Factory'),
+        title: const Text('요한의 블로그'),
         centerTitle: true,
-        actions: [
-          IconButton(
-            onPressed: () {
-              // http://
-              // https://
-              if(controller == null){
-                return;
-              }
-
-              controller!.loadUrl(homeUrl);
-            },
-            icon: Icon(
-              Icons.home,
-            ),
-          ),
-        ],
+        backgroundColor: Color(0xFF1E1E1E),
       ),
       body: WebView(
-        onWebViewCreated: (WebViewController controller) {
-          this.controller = controller;
-        },
-        initialUrl: homeUrl,
+        initialUrl: 'https://yohanpro.com',
         javascriptMode: JavascriptMode.unrestricted,
       ),
     );

e9e21cdd2128c859132064f75f532d107fac2939
Yohan Kim <yohan@yohanpro.com>
2023-01-24 21:45:18 +0900

    image PageView PageController 기능 추가

--- image_carousel/lib/screen/home_screen.dart
+++ image_carousel/lib/screen/home_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,10 +11,39 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Timer? timer;
+  PageController pageController = PageController(
+    initialPage: 0,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    timer = Timer.periodic(Duration(seconds: 4), (timer) {
+      int currentPage = pageController.page!.toInt();
+      int nextPage = currentPage + 1;
+
+      if (nextPage == 5) {
+        nextPage = 0;
+      }
+      pageController.animateToPage(nextPage,
+          duration: Duration(milliseconds: 400), curve: Curves.easeIn);
+    });
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    if (timer != null) timer!.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
     return Scaffold(
       body: PageView(
+        controller: pageController,
         children: List.generate(
           5,
           (index) => Image.asset(

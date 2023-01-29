cb244c3d22090c335c9e2b07d898db0f5c898b3b
Yohan Kim <yohan@yohanpro.com>
2023-01-29 13:39:00 +0900

    u_and i 프로젝트 init

---
+++ u_and_i/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    title: 'My app', // used by the OS task switcher
+    home: new Text('Hello World'),
+  ));
+}

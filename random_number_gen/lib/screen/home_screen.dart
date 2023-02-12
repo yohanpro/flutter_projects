a566ee28641d8331bc3885a27dd26c88b7c78754
Yohan Kim <yohan@yohanpro.com>
2023-02-12 20:54:05 +0900

    난수 생성하기

--- random_number_gen/lib/screen/home_screen.dart
+++ random_number_gen/lib/screen/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:random_number_gen/constants/color.dart';
 
@@ -9,6 +11,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<int> randomNumbers = [123, 456, 789];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,7 +46,7 @@ class _HomeScreenState extends State<HomeScreen> {
               Expanded(
                   child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: [123, 456, 789]
+                children: randomNumbers
                     .asMap()
                     .entries
                     .map((x) => Padding(
@@ -69,7 +73,19 @@ class _HomeScreenState extends State<HomeScreen> {
                       style: ElevatedButton.styleFrom(
                         backgroundColor: RED_COLOR,
                       ),
-                      onPressed: () {},
+                      onPressed: () {
+                        final rand = Random();
+                        final Set<int> newNumbers = {};
+
+                        while (newNumbers.length != 3) {
+                          final number = rand.nextInt(1000);
+                          newNumbers.add(number);
+                        }
+
+                        setState(() {
+                          randomNumbers = newNumbers.toList();
+                        });
+                      },
                       child: const Text('생성기')))
             ],
           ),

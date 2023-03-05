964be71fd68394405f0721664f47aba2da533f96
Yohan Kim <yohan@yohanpro.com>
2023-03-05 17:33:16 +0900

    동영상 플레이어 - HomeScreen 구현

---
+++ my_video_player/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import 'screen/home_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomeScreen(),
+  ));
+}
---
+++ my_video_player/lib/screen/home_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+          decoration: getBoxDecoration(),
+          child: SizedBox(
+            width: MediaQuery
+                .of(context)
+                .size
+                .width,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _Logo(),
+                SizedBox(
+                  height: 30.0,
+                ),
+                _AppName(),
+              ],
+            ),
+          ),
+        ));
+  }
+}
+
+BoxDecoration getBoxDecoration() {
+  return BoxDecoration(
+      gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [Color(0xFF2a4a7c), Color((0xFF000118))]));
+}
+
+class _Logo extends StatelessWidget {
+  const _Logo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset('asset/image/logo.png');
+  }
+}
+
+
+class _AppName extends StatelessWidget {
+  const _AppName({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = TextStyle(
+        color: Colors.white, fontWeight: FontWeight.w300, fontSize: 30.0);
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('Video'.toUpperCase(), style: textStyle),
+        Text('Player'.toUpperCase(),
+            style: textStyle.copyWith(fontWeight: FontWeight.w700))
+      ],
+    );
+  }
+}
